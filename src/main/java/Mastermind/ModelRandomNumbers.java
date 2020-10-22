7cd96c399d81bfbf98127675390968ee2bb4fe14
reigpol <69899286+reigpol@users.noreply.github.com>
2020-10-22 12:27:33 +0200

    Hem declarat la funció nextNumberi hem realitzat el testNextNumber que mira que tots els numeros creats aleatòriament siguin diferents els uns dels altres

---
+++ src/main/java/Mastermind/ModelRandomNumbers.java
@@ -0,0 +1,14 @@
+package Mastermind;
+
+import java.util.Random;
+
+public class ModelRandomNumbers {
+	//private static Random randomCreator;
+	private static int numRandom0, numRandom1, numRandom2, numRandom3;
+	static int [] arraySolution;
+	
+	Random randomCreator = new Random();
+	public static void nextNumber(Random randomCreator, int [] arraySolution) {
+		
+	}
+}

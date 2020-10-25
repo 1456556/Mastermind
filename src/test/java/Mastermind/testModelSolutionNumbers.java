cf1d4a099cd582d2ff8c1879f1cd49773e300c6b
Pol Reig <pol.reigp@e-campus.uab.cat>
2020-10-25 21:03:27 +0100

    Petita modificació de la funció testSolution de la classe TestModelSolutionNumbers on controlem el rang de números generats aleatòroament (que de moment estarà entre 0 i 9).

--- src/test/java/Mastermind/testModelSolutionNumbers.java
+++ src/test/java/Mastermind/testModelSolutionNumbers.java
@@ -17,5 +17,25 @@ public class TestModelSolutionNumbers {
 		assertNotEquals(arraySolution[1], arraySolution[2]);
 		assertNotEquals(arraySolution[1], arraySolution[3]);
 		assertNotEquals(arraySolution[2], arraySolution[3]);
+		
+		//Tots els 4 números generats han de ser més petits que 10
+		//i han de ser positius (més grans que 0)
+		boolean range0 = false;
+		boolean range1 = false;
+		boolean range2 = false;
+		boolean range3 = false;
+		if(arraySolution[0] < 10 && arraySolution[0] >= 0)
+			range0 = true;
+		if(arraySolution[1] < 10 && arraySolution[1] >= 0)
+			range1 = true;
+		if(arraySolution[2] < 10 && arraySolution[2] >= 0)
+			range2 = true;
+		if(arraySolution[3] < 10 && arraySolution[3] >= 0)
+			range3 = true;
+		
+		assertTrue(range0);
+		assertTrue(range1);
+		assertTrue(range2);
+		assertTrue(range3);
 	}
 }

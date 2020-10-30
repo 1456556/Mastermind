f3d59b3658eb4e7fd2d255eae558135fde9f1406
Angel Sacristán Ruiz <angelcnsw@gmail.com>
2020-10-30 09:22:55 +0100

    Menu
    Classe menu desenvolupada al 100%: creació dels atributs, creació del constructor, creació del metode pantalla que demana una opció i els getters/setters. Com es una vista no fa falta fer el test d'aquesta, ens han comentat que només fer test sobre els models que es on esta la 'xixa' i si es necessari també en el controlador

---
+++ src/main/java/views/Menu.java
@@ -0,0 +1,34 @@
+package views;
+
+import java.util.Scanner;
+
+public class Menu {
+	private int option;
+		
+		public Menu() {
+			this.option = 0;
+		}
+		
+		public void Pantalla() {
+			System.out.println("1. Iniciar el joc amb 10 intents");
+			System.out.println("2. Iniciar el joc amb 5 intents");
+			System.out.println("0. Sortir ");
+			System.out.println("");
+			System.out.println("Escull una opció: ");
+			
+			Scanner reader = new Scanner(System.in);
+			int selection = reader.nextInt();
+			
+			this.option = selection;
+		}
+
+		public int getOption() {
+			return option;
+		}
+
+		public void setOption(int option) {
+			this.option = option;
+		}
+		
+		
+}

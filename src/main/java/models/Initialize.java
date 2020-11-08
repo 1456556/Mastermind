c0ac20de7809ea425ceb3e1abdfb6bdd49e233b4
Angel Sacristán Ruiz <angelcnsw@gmail.com>
2020-11-08 20:23:24 +0100

    Optimització codi
    S'ha optimitzat el codi per al test case de initialize, també s'han introduit els caracters a l'array de resultats/intents de l'usuari i optimitzat el codi que ficava el resultat correcte de la partida

--- src/main/java/models/Initialize.java
+++ src/main/java/models/Initialize.java
@@ -8,40 +8,31 @@ public class Initialize {
 	private int attempt;
 	private Random randomCreator = new Random();
 	
-	public Initialize(int attempt) {
-		this.arrayAttempts = new String[attempt][6];
-		this.attempt = attempt;
+	public Initialize(int maxAttempt) {
+		this.arrayAttempts = new String[maxAttempt][6];
+		this.attempt = 1;
 		this.arraySolution = new int[4];
 	}
 	
 	public int[] solution() {
+		for(int index = 0; index < 4; index++) {
+			arraySolution[index] = Math.abs(randomCreator.nextInt()) % 10;
+			random();
+			System.out.println(arraySolution[index]);
+		}
 		
-		arraySolution[0] = Math.abs(randomCreator.nextInt()) % 10;
-		arraySolution[1] = Math.abs(randomCreator.nextInt()) % 10;
-		arraySolution[1] = random1();
-		arraySolution[2] = Math.abs(randomCreator.nextInt()) % 10;
-		arraySolution[2] = random2();
-		arraySolution[3] = Math.abs(randomCreator.nextInt()) % 10;
-		arraySolution[3] = random3();
 		return arraySolution;
 	}
 	
-	public int random1() {
+	public void random() {
 		while (arraySolution[1] == arraySolution[0])
 			arraySolution[1] = Math.abs(randomCreator.nextInt()) % 10;
-		return arraySolution[1];
-	}
-	
-	public int random2() {
+		
 		while (arraySolution[2] == arraySolution[1] || arraySolution[2] == arraySolution[0])
 			arraySolution[2] = Math.abs(randomCreator.nextInt()) % 10;
-		return arraySolution[2];
-	}
-	
-	public int random3() {
+		
 		while (arraySolution[3] == arraySolution[2] || arraySolution[3] == arraySolution[1] || arraySolution[3] == arraySolution[0])
 			arraySolution[3] = Math.abs(randomCreator.nextInt()) % 10;
-		return arraySolution[3];
 	}
 	
 	
@@ -58,15 +49,11 @@ public class Initialize {
         }
     }
 	
-	
-	//Aqui haurem de separar el string i posar els valors al arrayAttempts
-	//Hauriem de introduir per parametre tambe el arrayAttempts??
-	public boolean introduceArrayAttempts(String value) {
-		char[] valueSeparate = value.toCharArray();
-		arrayAttempts[attempt][0] = 
-		return true;
+	public void introduceArrayAttempts(String value) {
+		
 	}
 
+	
 
 	public int[] getArraySolution() {
 		return arraySolution;
--- src/test/java/tests/InitializeTest.java
+++ src/test/java/tests/InitializeTest.java
@@ -14,7 +14,6 @@ public class InitializeTest {
 		mrn.solution();
 		arraySolution = mrn.getArraySolution();
 		
-		//Tots els 4 números han de ser diferents entre ells
 		assertNotEquals(arraySolution[0], arraySolution[1]);
 		assertNotEquals(arraySolution[0], arraySolution[2]);
 		assertNotEquals(arraySolution[0], arraySolution[3]);
@@ -22,53 +21,25 @@ public class InitializeTest {
 		assertNotEquals(arraySolution[1], arraySolution[3]);
 		assertNotEquals(arraySolution[2], arraySolution[3]);
 		
-		//Tots els 4 números generats han de ser més petits que 10
-		//i han de ser positius (més grans que 0)
-		boolean range0 = false;
-		boolean range1 = false;
-		boolean range2 = false;
-		boolean range3 = false;
-		if(arraySolution[0] < 10 && arraySolution[0] >= 0)
-			range0 = true;
-		if(arraySolution[1] < 10 && arraySolution[1] >= 0)
-			range1 = true;
-		if(arraySolution[2] < 10 && arraySolution[2] >= 0)
-			range2 = true;
-		if(arraySolution[3] < 10 && arraySolution[3] >= 0)
-			range3 = true;
-		
-		assertTrue(range0);
-		assertTrue(range1);
-		assertTrue(range2);
-		assertTrue(range3);
+		boolean range = false;
+		for(int index = 0; index < 4; index++) {
+			if(arraySolution[index] < 10 && arraySolution[index] >= 0)
+				range = true; 
+			assertTrue(range);
+		}
 	}
 	
 	
 	
-	// Comprobar que els valors introduits son numeros entre 0 y 9, y tambe diferents entre ells
 	@Test public void testCheckNumber() {
-        //Cridem al metode MockTeclat() per tal de que introdueixi un valor (value)
-		//A continuacio fem el test
-		
-		MockTeclat mt = new MockTeclat("1", "1234");
-		String value = mt.getSelection();
-		boolean checkNum = true;
-		if(value.length() != 4) {
-			//checkNum = false;
-        }else {
-            try {
-                Integer.parseInt(value);
-                checkNum = true;
-            } catch (NumberFormatException e) {
-            	//checkNum = false;
-            }
-        }
+		Initialize mrn = new Initialize(10);
+		boolean checkNum = mrn.checkNumber("1234");
 		assertTrue(checkNum);
 	}
 	
-	// Comprobar que s'introdueixen els valors correctament a l'array
+
 	@Test public void testIntroduceArrayAttempts() {
-		
+		// HACER TEST introduceArrayAttempts() -> Preguntar profe
 	}
 	
 }
